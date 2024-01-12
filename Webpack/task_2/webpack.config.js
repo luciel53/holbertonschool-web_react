04613572e1f0629fda764f32e6f0c28f8a8dc38c
luciel53 <l.leroty@gmail.com>
2024-01-12 15:02:59 +0100

    task 2

---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/dashboard_main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/', // Adjust the output path as needed
+            },
+          },
+        ],
+      },
+    ]
+  }
+};
