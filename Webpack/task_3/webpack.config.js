22d68e41481869c9ea15c5a442ec8e83d0f8cfde
luciel53 <l.leroty@gmail.com>
2024-01-12 19:57:47 +0100

    task 3

--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 
 module.exports = {
   entry: {
@@ -41,6 +43,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html'),
     }),
+    new CleanWebpackPlugin(),
   ],
   performance: { // to fix warning size
     hints: false,
