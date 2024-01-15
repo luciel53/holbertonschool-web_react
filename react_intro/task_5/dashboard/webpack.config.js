111830d6de79c5e07d0ee75d6d437fe32c748468
luciel53 <l.leroty@gmail.com>
2024-01-15 20:19:41 +0100

    task 8

---
+++ react_intro/task_5/dashboard/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
+    hot: true,
+  },
+  mode: 'development',
+};
